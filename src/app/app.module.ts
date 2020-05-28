3d74cc1eda7295dc405c118a2b7ba71f5dac4d0a
luiza <luizaugusto3b@gmail.com>
2020-05-28 21:02:54 +0200

    adding shares and charts components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SharesComponent } from './shares/shares.component';
+import { ChartsComponent } from './charts/charts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SharesComponent,
+    ChartsComponent
   ],
   imports: [
     BrowserModule
